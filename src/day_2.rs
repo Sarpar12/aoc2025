0615bde79239520d785611cff0012ca4c0603acf
Ashton Dong <dong.ashton@gmail.com>
2025-12-02 01:20:06 -0500

    Day 2 initial

---
+++ src/day_2.rs
@@ -0,0 +1,12 @@
+/// sums all numbers that contains two copies of a number inside
+/// ie: 11, 22, 1212
+pub fn part1(input: &str) -> Result<i64, std::io::Error>{
+    let sum: i64 = 0;
+    
+    
+    Ok(sum)
+}
+
+pub fn part2(input: &str) {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod day_1;
+mod day_2;
 
 fn main() {
     // Part 1
-    let p1_out = match day_1::part1("./src/Inputs/D1I1.txt") {
+    let p1_out = match day_2::part1("./src/Inputs/D1I1.txt") {
         Ok(value) => value,
         Err(e) => panic!("Error: {}", e),
     };
     println!("Part 1 output: {p1_out}!");
     // Part 2
-    let p2_out = match day_1::part2("./src/Inputs/D1I1.txt") {
+    let p2_out = match day_2::part2("./src/Inputs/D1I1.txt") {
         Ok(value) => value,
         Err(e) => panic!("Error: {}", e),
     };
