c783ce7c33fa81dde4974bff6f2573c0f4ac76de
Ashton Dong <dong.ashton@gmail.com>
2025-12-02 01:42:26 -0500

    Day 2 Part 1

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,12 +1,40 @@
+use std::fs;
+
+fn read_input(input: &str) -> Result<Vec<(i64, i64)>, std::io::Error> {
+    let contents = fs::read_to_string(input)?;
+    let vals: Vec<(i64, i64)> = contents
+        .split(',')
+        .map(|term| {
+            let parts: Vec<&str> = term.trim().split('-').collect();
+            let a = parts[0].parse::<i64>().unwrap();
+            let b = parts[1].parse::<i64>().unwrap();
+            (a, b)
+        })
+        .collect();
+    Ok(vals)
+}
+
 /// sums all numbers that contains two copies of a number inside
 /// ie: 11, 22, 1212
-pub fn part1(input: &str) -> Result<i64, std::io::Error>{
-    let sum: i64 = 0;
-    
-    
+pub fn part1(input: &str) -> Result<i64, std::io::Error> {
+    let mut sum: i64 = 0;
+    let vals = read_input(input)?;
+    for (start, end) in vals {
+        for num in start..=end {
+            let digits = num.ilog10() + 1;
+            if digits % 2 != 0 {
+                continue;
+            }
+            let half_digits = digits / 2;
+            let divisor = 10i64.pow(half_digits);
+            let second_half = num % divisor;
+
+            if (num / divisor) == second_half {
+                sum += num;
+            }
+        }
+    }
     Ok(sum)
 }
 
-pub fn part2(input: &str) {
-    
-}
+pub fn part2(input: &str) {}
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod day_2;
 
 fn main() {
     // Part 1
-    let p1_out = match day_2::part1("./src/Inputs/D1I1.txt") {
+    let p1_out = match day_2::part1("./src/Inputs/D2.txt") {
         Ok(value) => value,
         Err(e) => panic!("Error: {}", e),
     };
     println!("Part 1 output: {p1_out}!");
     // Part 2
-    let p2_out = match day_2::part2("./src/Inputs/D1I1.txt") {
+    let p2_out = match day_1::part2("./src/Inputs/D1I1.txt") {
         Ok(value) => value,
         Err(e) => panic!("Error: {}", e),
     };
